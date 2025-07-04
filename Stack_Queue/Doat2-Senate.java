4d6455bfdf2d42051a654d4320247cabcb9a33a1
Aryan Rai <aryanrai97861@gmail.com>
2025-07-05 05:23:57 +0530

    Queue

---
+++ Stack_Queue/Doat2-Senate.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String predictPartyVictory(String senate) {
+        Queue<Integer> radiant=new LinkedList<>();
+        Queue<Integer> dire=new LinkedList<>();
+
+        for(int i=0;i<senate.length();i++){
+            if(senate.charAt(i)=='R') radiant.offer(i);
+            else dire.offer(i);
+        }
+        while(!radiant.isEmpty() && !dire.isEmpty()){
+            int r=radiant.poll();
+            int d=dire.poll();
+            if(r<d){
+                radiant.offer(r+senate.length());
+            }else{
+                dire.offer(d+senate.length());
+            }
+        }
+        if(!radiant.isEmpty()){
+            return "Radiant";
+        }else{
+            return "Dire";
+        }
+    }
+}
