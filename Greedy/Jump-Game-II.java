2c082795b8c85e5ff8eb01a8ed6a286a732e7d8b
Aryan Rai <aryanrai97861@gmail.com>
2025-07-09 20:43:38 +0530

    greedy

---
+++ Greedy/Jump-Game-II.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int jump(int[] nums) {
+        int steps = 0;
+        int end = 0;
+        int farthest = 0;
+        
+        for (int i = 0; i < nums.length - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+            if (i == end) {
+                steps++;
+                end = farthest;
+            }
+        }
+        return steps;
+    }
+}
